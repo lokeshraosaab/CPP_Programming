8190c85efbea4cd2eaf40ac18a04a8f72c04d83d
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-08-30 02:37:14 +0530

    Initial Commit

---
+++ 01_intro_cpp/01_first_program.cpp
@@ -0,0 +1,10 @@
+# include<iostream>
+
+//using namespace std;
+// use -->   using namespace std;  Or std:: before cout 
+// your program can run with use of both at the same or out of one at a time more better.
+
+int main(){
+    std::cout<<"Lokesh Rao Sahab";
+    return 0;
+}
---
+++ 01_intro_cpp/02_comments.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+
+using namespace std;
+
+// This is a single line comment.
+
+/* This
+is a 
+multiline
+comment*/
+
+int main(){
+    int a = 6;
+    cout<<"Hello World \n";
+    cout<<a;
+    return 0;
+}
