db64cb3b8c5e938ac2a01cb2a0fe7b83a6b3c667
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-02 22:41:42 +0530

    Initial Commit

---
+++ 03_operators_and_typecasting/03_assignment_op.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+int a=4;
+float b=3.3;
+char c='u';
+// here = is used to assign values to variables a, b, c so(=) is called assignment operator.
+cout<<"The value of the variables a, b, and c is "<<a<<", "<<b<<", and "<<c<<" respectively.";
+     return 0;
+}
