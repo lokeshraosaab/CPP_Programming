9fcc211e78dd01e97b4336bf0f352dc87923f288
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-02 22:37:25 +0530

    Initial Commit

---
+++ 02_variables/04_i_o_stream.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+int num1, num2;
+cout<<"Enter the value of num1: \n";
+cin>>num1;
+cout<<"Enter the value of num2: \n";
+cin>>num2;
+cout<<"The value of num1 is: "<<num1<<"\n";
+cout<<"The value of num2 is: "<<num2<<"\n";
+cout<<"The value of sum of num1 and num2 is: "<<num1+num2<<"\n";
+
+     return 0;
+}
