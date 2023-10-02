8e67509facba375088add35761de567a937a2ec3
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-02 22:41:18 +0530

    Initial Commit

---
+++ 03_operators_and_typecasting/02_arithmetic_operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+int a=10, b=6;
+float c=4.6, d=2.1;
+cout<<"The value of a+b is: "<<a+b<<endl;
+cout<<"The value of a-b is: "<<a-b<<endl;
+cout<<"The value of a*b is: "<<a*b<<endl;
+cout<<"The value of a/b is: "<<a/b<<endl;
+cout<<"The value of c/d is: "<<c/d<<endl;
+cout<<"The value of a%b is: "<<a%b<<endl;
+cout<<"The value of a++ is: "<<a++<<endl;
+cout<<"The value of a-- is: "<<a--<<endl;
+cout<<"The value of ++a is: "<<++a<<endl;
+cout<<"The value of --a is: "<<--a<<endl;
+     return 0;
+}
