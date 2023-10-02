383a1a909131d96c80e4f2255f8bd8a3b24146c6
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-02 22:40:49 +0530

    Initial Commit

---
+++ 03_operators_and_typecasting/01_header_files.cpp
@@ -0,0 +1,9 @@
+#include<iostream> // --> It comes with the compiler . It is System Header File.
+using namespace std ;
+#include"this.h" //--> It is written by the programmer . firstly should be create in our directory. 
+// otherwise it will produce an error.
+int main()
+{
+cout<<"This is header file program\n";
+     return 0;
+}
