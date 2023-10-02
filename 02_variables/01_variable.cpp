b643fbfa99824b07cefe3b409d029d7e8e0c1764
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-02 22:34:02 +0530

    Initial Commit

---
+++ 02_variables/01_variable.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=4, b=9;
+    float pi=3.14;
+    char c = 'u';
+    bool d = true;
+    // bool = true; --> displays 1 for true 
+    // bool = false; --> displays 0 for false
+    cout<< "The value of the variable a is "<<a<<".\n";
+    cout<< "The value of the variable b is "<<b<<".\n";
+    cout<< "The value of the variable pi is "<<pi<<".\n";
+    cout<< "The value of the variable c is "<<c<<".\n";
+    cout<< "The value of the variable d is "<<d<<".\n";
+
+     return 0;
+}
