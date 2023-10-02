8ea661b4bd6eed7e433ade87c54707e689ee8508
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-02 22:42:13 +0530

    Initial Commit

---
+++ 03_operators_and_typecasting/04_comparison_op.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+int a=4, b=5;
+// if the given comparison is true it returns 1 and if the given comparison is false it returns 0.
+cout<<"The value of the comparison (a==b) is "<<(a==b)<<endl;
+cout<<"The value of the comparison (a!=b) is "<<(a!=b)<<endl;
+cout<<"The value of the comparison (a>=b) is "<<(a>=b)<<endl;
+cout<<"The value of the comparison (a<=b) is "<<(a<=b)<<endl;
+cout<<"The value of the comparison (a>b) is "<<(a>b)<<endl;
+cout<<"The value of the comparison (a<b) is "<<(a<b)<<endl;
+     return 0;
+}
