22d8e9b0158e826090b80198e80536f7a6100c24
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-05 16:08:28 +0530

    Initial Commit

---
+++ 04_control_structures/06_macros.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+// macros definition ...
+#define LIMIT 5  // no need a semi-colon (;) to end ....
+
+int main()
+{
+    for(int i=0; i<LIMIT; i++){
+        cout<<i<<endl;
+    }
+     return 0;
+}
