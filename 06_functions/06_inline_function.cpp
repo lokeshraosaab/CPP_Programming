aeb93a79de90a7ab116d37b14d3c909c95a14c26
Lokesh Kumar <lokeshkumar797877@gmail.com>
2024-02-25 23:35:59 +0530

    Initial Commit

---
+++ 06_functions/06_inline_function.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std ;
+
+inline int product(int a, int b){
+    return a*b;
+}
+
+int main()
+{
+    int a, b;
+    cout<<"Enter the value of a: "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b: "<<endl;
+    cin>>b;
+    cout<<"The product of a and b is : "<<product(a,b)<<endl;
+    cout<<"The product of a and b is : "<<product(a,b)<<endl;
+    cout<<"The product of a and b is : "<<product(a,b)<<endl;
+    cout<<"The product of a and b is : "<<product(a,b)<<endl;
+    cout<<"The product of a and b is : "<<product(a,b)<<endl;
+    cout<<"The product of a and b is : "<<product(a,b)<<endl;
+    cout<<"The product of a and b is : "<<product(a,b)<<endl;
+     return 0;
+}
