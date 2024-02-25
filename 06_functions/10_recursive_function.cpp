887ff6a4060292ce5b07f11d9d5274333f01455c
Lokesh Kumar <lokeshkumar797877@gmail.com>
2024-02-25 23:38:39 +0530

    Initial Commit

---
+++ 06_functions/10_recursive_function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    // Recursion : A defined fuction can call itself is called recursion....
+    // Recursive function : A function calling itself is also called recursive function....
+
+    // Examples.....Based on..... 
+    //factorial(n) = n * factorial(n-1);
+    // Sum of first n natural numbers.....sum(n) = n + sum(n-1);
+    // nth term of fibonacci series....fibonacci(n) = fibonacci(n-1) + fibonacci(n-2);
+
+    // To solve these problems we have to decide base conditions.....
+     return 0;
+}
